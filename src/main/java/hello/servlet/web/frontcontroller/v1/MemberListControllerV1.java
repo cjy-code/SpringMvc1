f802bc193f51ec39a27539b1691802deb1145abb
jychoi <ggozxc@gmail.com>
2022-08-17 23:37:50 +0900

    study_commit_ver1.4

--- src/main/java/hello/servlet/web/frontcontroller/v1/MemberListControllerV1.java
+++ src/main/java/hello/servlet/web/frontcontroller/v1/MemberListControllerV1.java
@@ -1,28 +1,22 @@
-package hello.servlet.web.serveletmvc;
+package hello.servlet.web.frontcontroller.v1;
 
 import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet(name="mvcMemberListServlet", urlPatterns = "/servlet-mvc/members")
-public class MvcMemberListServlet extends HttpServlet {
+public class MemberListControllerV1 implements ControllerV1{
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
     @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        System.out.println("MvcMemberListServlet.service");
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         List<Member> members = memberRepository.findAll();
-
         request.setAttribute("members", members);
         String viewPath = "/WEB-INF/views/members.jsp";
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
